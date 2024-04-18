531887b39eff3b879755a6363f828414f8aa25d7
Mindaugas Krivickas <mkrivickas@gmail.com>
2024-04-18 19:18:55 +0300

    finished dynamic temperature feature

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 async function start() {
   const weatherPromise = await fetch(
-    "https://api.weather.gov/gridpoints/MFL/110,50/forecast"
+    // "https://api.weather.gov/gridpoints/MFL/110,50/forecast"
+    "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=apparent_temperature"
   );
   const weatherData = await weatherPromise.json();
-  const ourTemperature = weatherData.properties.periods[0].temperature;
-  console.log(ourTemperature);
+  // const ourTemperature = weatherData.properties.periods[0].temperature;
+  const ourTemperature = weatherData.hourly.apparent_temperature[12];
+  document.querySelector("#temperature-output").textContent = ourTemperature;
 }
 
 start();
