1eb8394cdc6864edc7cac7ce1ce56ee531164d53
Mindaugas Krivickas <mkrivickas@gmail.com>
2024-04-18 19:50:49 +0300

    work in progress, feched pet data

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 async function start() {
   const weatherPromise = await fetch(
-    // "https://api.weather.gov/gridpoints/MFL/110,50/forecast"
     "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=apparent_temperature"
   );
   const weatherData = await weatherPromise.json();
-  // const ourTemperature = weatherData.properties.periods[0].temperature;
   const ourTemperature = weatherData.hourly.apparent_temperature[12];
   document.querySelector("#temperature-output").textContent = ourTemperature;
 }
 
 start();
+
+async function petsArea() {
+  const petsPromise = await fetch(
+    "https://learnwebcode.github.io/bootcamp-pet-data/pets.json"
+  );
+  const petsData = await petsPromise.json();
+  petsData.forEach((pet) => {
+    console.log(pet.species);
+  });
+}
+
+petsArea();
